4df938e404ddfb67db71280a27f37a49ef8cfac3
Ryan Jowers <rjowers@g.clemson.edu>
2017-03-15 15:22:42 -0400

    created home page and add contacts page

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+
+
+
+import angular from 'angular';
+import 'angular-ui-router';
+
+import AppConfig from './config';
+
+// import AnimalController from './controllers/animal';
+// import ShelterController from './controllers/shelter';
+
+angular
+  .module('contact-app', ['ui.router'])
+  .config(AppConfig)
+  // .controller('ShelterController', ShelterController)
+  // .controller('AnimalController', AnimalController);
