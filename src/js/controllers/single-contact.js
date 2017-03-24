b2b6f9430c6e429e95e93ce4f5cf8cdd4782ae4c
Ryan Jowers <rjowers@g.clemson.edu>
2017-03-23 21:07:16 -0400

    finished styling

--- src/js/controllers/single-contact.js
+++ src/js/controllers/single-contact.js
@@ -1,14 +1,18 @@
 const SERVER_URL = "https://ancient-stream-35750.herokuapp.com";
 
-function SingleContactController ($scope, $http, $state) {
-  $http.get(`${SERVER_URL}/contacts`).then(resp => {
+function SingleContactController ($scope, $http, $stateParams) {
 
+var contactid = $stateParams.id;
+
+  $http.get(`${SERVER_URL}/contacts/${contactid}`).then(resp => {
+        // for (i = 0; i < data.length; i++)
         $scope.contact = resp.data;
-        // console.log(resp.data[]);
+        console.log(resp.data);
+
 
 
 });
 }
-SingleContactController.$inject = ['$scope', '$http', '$state'];
+SingleContactController.$inject = ['$scope', '$http', '$stateParams'];
 
  export default SingleContactController;
